59462ad34a0b3bd81855c0bb5186f0a5a197335b
Ogpavan <pawantwp@gmail.com>
2024-10-06 20:26:56 +0530

    Vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://page-fm-backend.vercel.app', // Replace with your desired base URL
+  base: '/', // Replace with your desired base URL
 });
