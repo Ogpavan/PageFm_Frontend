bdcc9b6ed29ec2cd3bcfd458d7eeb1d2231720b0
Ogpavan <pawantwp@gmail.com>
2024-11-07 12:24:37 +0530

    first

--- src/components/EpisodeDetails.jsx
+++ src/components/EpisodeDetails.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Loader from './Loader';
+import { GoArrowRight } from "react-icons/go";
 
 const EpisodeDetails = ({ setShowNavbar, setShowFooter }) => {
   const { episodeId, bookId } = useParams();
@@ -9,9 +10,7 @@ const EpisodeDetails = ({ setShowNavbar, setShowFooter }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [book, setBook] = useState(null);
-
-  // Hide Navbar and Footer when this component is mounted
- 
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchBookAndEpisode = async () => {
@@ -35,6 +34,18 @@ const EpisodeDetails = ({ setShowNavbar, setShowFooter }) => {
     fetchBookAndEpisode();
   }, [episodeId, bookId]);
 
+  const handleNextChapter = () => {
+    if (book && episode) {
+      const currentIndex = book.episodes.findIndex(ep => ep._id === episodeId);
+      if (currentIndex >= 0 && currentIndex < book.episodes.length - 1) {
+        const nextEpisodeId = book.episodes[currentIndex + 1]._id;
+        navigate(`/books/${bookId}/episodes/${nextEpisodeId}`);
+      } else {
+        alert('This is the last chapter.');
+      }
+    }
+  };
+
   if (loading) {
     return <Loader />;
   }
@@ -49,8 +60,21 @@ const EpisodeDetails = ({ setShowNavbar, setShowFooter }) => {
 
   return (
     <div className="max-w-2xl mx-auto px-6 py-12 bg-gray-50 text-gray-800 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] rounded-lg">
-      <h1 className="text-4xl font-bold text-center mb-6"><p> Chapter {episode.episodeNumber}</p><p className='text-xl mt-3'>{episode.title}</p></h1>
+      <h1 className="text-4xl font-bold text-center mb-6">
+        <p>Chapter {episode.episodeNumber}</p>
+        <p className="text-xl mt-3">{episode.title}</p>
+      </h1>
       <div className="mt-4 prose lg:prose-lg leading-relaxed text-justify" dangerouslySetInnerHTML={{ __html: episode.content }} />
+      <div className='w-full flex justify-end items-center'>
+      <button
+        onClick={handleNextChapter}
+        className="mt-8  border   py-3 px-6  font-semibold text-right
+         rounded-lg  transition duration-300 flex items-center justify-end"
+      >
+        <span className='text-sm'>
+        Next Chapter</span><span className='ml-2'><GoArrowRight /></span>
+      </button>
+    </div>
     </div>
   );
 };
